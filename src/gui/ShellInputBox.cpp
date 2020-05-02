f76ceb1a8be1f58633eb9ec33c71428adbd24e01
Florian Schieder <florian.schieder@web.de>
2020-05-02 11:26:55 +0200

    Themed ShellInputBox

--- src/gui/ShellInputBox.cpp
+++ src/gui/ShellInputBox.cpp
@@ -7,7 +7,13 @@ METHOD ShellInputBox::ShellInputBox(Window* parent, int x, int y, int width, int
 METHOD void ShellInputBox::Show()
 {
     InputBox::Show();
-    HRESULT r = SHAutoComplete(
+
+    SHAutoComplete(
         this->m_controlHandle,
         SHACF_FILESYS_DIRS | SHACF_AUTOSUGGEST_FORCE_ON);
+
+    SetWindowTheme(
+        this->m_controlHandle,
+        L"Explorer",
+        NULL);
 }
