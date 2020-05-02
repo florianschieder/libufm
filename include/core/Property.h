43c15267ad77b45e5209fed3f51ebbe7ebf1de4a
Florian Schieder <florian.schieder@web.de>
2020-05-02 12:01:23 +0200

    Added Property class

---
+++ include/core/Property.h
@@ -0,0 +1,43 @@
+template <typename T> class ReadOnlyProperty
+{
+    public:
+        ReadOnlyProperty() {}
+        ReadOnlyProperty(T initialValue)
+        {
+            this->value = initialValue;
+        }
+
+        operator T()
+        {
+            return this->getValue();
+        }
+
+    private:
+        T getValue()
+        {
+            return this->value;
+        }
+    protected:
+        T value;
+};
+
+template <typename T> class Property
+    : public ReadOnlyProperty<T>
+{
+    public:
+        Property() 
+            : ReadOnlyProperty<T>() {}
+        Property(T initialValue)
+            : ReadOnlyProperty<T>(initialValue) {}
+
+        void operator=(T newValue)
+        {
+            this->setValue(newValue);
+        }
+
+    private:
+        void setValue(T newValue)
+        {
+            this->value = newValue;
+        }
+};
